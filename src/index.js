03d03ed5ec311ccb7acad2cecd2249ee12d27772
Florin Onciu <florin.cosmin@fastmail.com>
2019-04-29 22:23:26 +0200

    wip on firebase app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,67 @@
 import { ready } from "./js/utils";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.scss';
-import App from './js/App';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import * as firebase from "firebase/app";
+import { firebaseProject, listenForAuthChanges } from "./js/firebase-setup";
+
+const Menu = ({ title, onClick }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand">Firebase Demo</a>
+      <button
+        className="btn btn-outline-success"
+        type="submit"
+        onClick={onClick}
+      >
+        {title}
+      </button>
+    </nav>
+  );
+};
+
+class FirebaseApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authState: null
+    };
+
+    listenForAuthChanges(authState => {
+      this.setState({ authState });
+    });
+  }
+
+  signIn() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  signOut() {
+    firebase.auth().signOut();
+  }
+
+  render() {
+    const isLoggedIn = !!this.state.authState;
+    const status = isLoggedIn ? this.state.authState.userName : "firebase demo";
+
+    return (
+      <>
+        <Menu
+          title={isLoggedIn ? "Logout" : "Login"}
+          onClick={isLoggedIn ? this.signOut : this.signIn}
+        />
+        <div className="container">
+          <p>Hello {status}!</p>
+        </div>
+      </>
+    );
+  }
+}
 
 ready(() => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-})
+  // const database = firebaseProject.database();
+  // console.log(database);
+
+  ReactDOM.render(<FirebaseApp />, document.getElementById("root"));
+});
---
+++ src/js/firebase-setup.js
@@ -0,0 +1,67 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/database";
+
+const _onAuthStateChangedObservers = [];
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC76nSpYoSG1JKUlmOmcHFU17TyFrb_Yj8",
+  authDomain: "fcc-todo-project.firebaseapp.com",
+  databaseURL: "https://fcc-todo-project.firebaseio.com",
+  projectId: "fcc-todo-project",
+  storageBucket: "fcc-todo-project.appspot.com",
+  messagingSenderId: "989312702341"
+};
+
+// Initialize Firebase
+const firebaseProject = firebase.initializeApp(firebaseConfig);
+
+// listen for auth state changes
+firebase.auth().onAuthStateChanged(authStateObserver);
+
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  return firebase.auth().currentUser.photoURL;
+}
+
+// Returns the signed-in user's display name.
+function getUserName() {
+  return firebase.auth().currentUser.displayName;
+}
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  return !!firebase.auth().currentUser;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  // no listeners
+  if (_onAuthStateChangedObservers.length === 0) {
+    return;
+  }
+  if (user) {
+    // User is signed in!
+    const profilePicUrl = getProfilePicUrl();
+    const userName = getUserName();
+
+    // notify every listener about login state
+    _onAuthStateChangedObservers.map(callback => {
+      callback({ userName, profilePicUrl });
+    });
+  } else {
+    // notify every listener about logout state
+    _onAuthStateChangedObservers.map(callback => {
+      callback();
+    });
+  }
+}
+
+function listenForAuthChanges(callback) {
+  _onAuthStateChangedObservers.push(callback);
+}
+
+export { firebaseProject, listenForAuthChanges, isUserSignedIn };
